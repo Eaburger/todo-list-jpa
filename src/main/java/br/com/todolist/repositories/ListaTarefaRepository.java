ecc28206d9c7fc1b293e9e9d0ad51c993a93debc
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-11-29 15:55:25 -0300

    Adicionando repositoro de tarefas

---
+++ src/main/java/br/com/todolist/repositories/ListaTarefaRepository.java
@@ -0,0 +1,33 @@
+package br.com.todolist.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import org.w3c.dom.Entity;
+
+import br.com.todolist.model.ListaTarefa;
+
+public class ListaTarefaRepository implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EntityManager entityManager;
+	
+	public List<ListaTarefa> listaTudo(){
+		
+		return entityManager.createQuery("from lista_tarefa", ListaTarefa.class).getResultList();
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
